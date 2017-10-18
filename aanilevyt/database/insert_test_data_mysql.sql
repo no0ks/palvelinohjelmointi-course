16f3f1315c9deb0f1d4630848ef5f92840694dfa
Nooki <nooki@Willow.local>
2017-10-18 13:26:46 +0300

    Modified test data

--- aanilevyt/database/insert_test_data_mysql.sql
+++ aanilevyt/database/insert_test_data_mysql.sql
@@ -3,4 +3,9 @@ INSERT INTO aanilevyt
 VALUES
 	('Playing the Angel', 'Depeche Mode', 2005),
 	('Aura', 'CMX', 1994),
+	('Parklife', 'Blur', 1994),
+	('Speak & Spell', 'Depeche Mode', 1981),
+	('Dark Adrenaline', 'Lacuna Coil', 2012),
+	('This Is Hardcore', 'Pulp', 1998),
+	('Mutter', 'Rammstein', 2001),
 	('Epicloud', 'The Devin Townsend Project', 2012);
